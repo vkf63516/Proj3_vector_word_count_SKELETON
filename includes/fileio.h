7ecf45ef5c0685f8df9bb7f5abbc0f66b3899cdb
keith <keith.perkins@cnu.edu>
2020-10-08 21:48:09 -0400

    fixed comment

--- includes/fileio.h
+++ includes/fileio.h
@@ -25,7 +25,7 @@ void closeFile(std::fstream& myfile);
 /* serializes all content in entries to file outputfilename
  * check out utils for helpful type conversion functions
  * returns  FAIL_FILE_DID_NOT_OPEN if cannot open outputfilename
- * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
+ * 			FAIL_NO_ARRAY_DATA if there are 0 entries in entries
  * 			SUCCESS if all data is written and outputfilename closes OK
  * */
 int writetoFile(std::vector<constants::entry>  &entries, const std::string &outputfilename);
